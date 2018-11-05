93133e9c5859f047f69261e406c105f3c48c4ac8
admin <2237820489@qq.com>
2018-11-05 22:06:06 +0800

    修改项目到git

--- src/main/java/com/itheima/git/GitTest.java
+++ src/main/java/com/itheima/git/GitTest.java
@@ -3,5 +3,6 @@ package com.itheima.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("执行git---------------------");
+        System.out.println("执行git---------------------");
     }
 }
